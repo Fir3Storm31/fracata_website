43438a8d14ddac6b931885ea197943577294a947
Fir3Storm31 <francesco.catalanotti25>
2025-07-12 10:52:08 +0200

    Added projects page

--- app.js
+++ app.js
@@ -60,6 +60,13 @@ app.get('/books', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects', { 
+        title: 'Projects',
+        active: 'projects'
+    });
+});
+
 // Handle 404
 app.use((req, res) => {
     res.status(404).render('404', {
